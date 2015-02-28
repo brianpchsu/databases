ff41e2a3454a849162eb9f4fe84c9ff7c65559bb
brianpchsu <brianpchsu@gmail.com>
2015-02-27 19:36:35 -0800

    GET request retrieves messages in MySql

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -13,18 +13,19 @@ dbConnection.connect();
 module.exports = {
   messages: {
     get: function (req, res) {
-      res.writeHead(201);
-      var message = {objectId: 1, roomname: 'lobby', text: 'lol bbq', username: 'me'};
-      var data = {results: [message]};
-
-      res.end(JSON.stringify(data));
+      dbConnection.query('SELECT * FROM messages', function(err, data){
+        res.writeHead(201);
+        res.end(JSON.stringify({results: data}));
+      });
 
       //res.send({result: ???});
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       console.log('hitting');
       req.body.username = req.body.username || 'Annonymous';
-      dbConnection.query('INSERT INTO messages (username, roomname, message) values ("' + req.body.username + '", "' + req.body.roomname + '", "' + req.body.text + '");');
+      dbConnection.query('INSERT INTO messages ' +
+        ' (username, roomname, message) values ' +
+        '("' + req.body.username + '", "' + req.body.roomname + '", "' + req.body.text + '");');
 
     } // a function which handles posting a message to the database
   },
